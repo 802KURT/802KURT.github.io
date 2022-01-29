6865a572ff511bd184ddba37401ed01314ad8cbb
802KURT <98626503+802KURT@users.noreply.github.com>
2022-01-29 01:37:29 -0600

    First Edit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+/*!
+ * Item: Kitzu
+ * Description: Personal Portfolio Template
+ * Author/Developer: Exill
+ * Author/Developer URL: https://themeforest.net/user/exill
+ * Version: v2.0.0
+ * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ */
+
+/*----------- CUSTOM JS SCRIPTS -----------*/
+
+(function($) {
+  'use strict';
+  $(function() {
+    // Code here executes When the DOM is loaded...
+  });
+  $(window).on('load', function() {
+    // Code here executes When the page is loaded
+  });
+}(jQuery));
+
+
+function playrain() {
+  var audio = document.getElementById("track");
+  audio.play();
+}
+function stoprain() {
+  var audio = document.getElementById("track");
+  audio.pause();
+}
